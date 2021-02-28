a77ec43a83386473be731eb0dcce7cd725d2040b
Yaris van Thiel <yarisvanthiel@gmail.com>
2021-02-28 14:42:10 +0100

    Begin adding cumulative sum

--- api/src/routes/getMessages.js
+++ api/src/routes/getMessages.js
@@ -8,8 +8,28 @@ module.exports = {
     path: '/api/messages', // ?words=[A-Za-z0-9, ]+&monthly=true|false
     method: 'get',
     middlewares: [],
-    readRequest: async (req, res) => {
-        if (req.query.monthly === 'true') {
+    readRequest: async (req, res) => {  
+        if (req.query.monthly === 'true' && req.query.cumsum === 'true') {
+            if (req.query.words) {
+                if (req.query.words) {
+                    sequelize.query('SELECT DATE_FORMAT(time, \'%Y\') AS year, DATE_FORMAT(time, \'%c\') AS month, name, COUNT(*) as count, SUM(count(p.id)) OVER (PARTITION BY name ORDER BY YEAR(time), MONTH(time)) AS cumsum ' +
+                                    'FROM Messages as m, People as p, Words as w, MessageWords as mw ' +
+                                    'WHERE p.id = m.PersonId AND m.id = mw.MessageId AND mw.WordId = w.id AND w.word IN (:words) ' +
+                                    'GROUP BY PersonId, YEAR(time), MONTH(time)', { 
+                        replacements: {
+                            words: req.query.words.split(',')
+                        },
+                        type: QueryTypes.SELECT
+                    }).then((result) => {
+                        res.status(200).json(new Response(true, sqlResultToTimeSeriesLineChart(result)));
+                    }).catch((err) => {
+                        consola.error(err);
+                        res.status(500).json(new Response(false, 'An error occured quering the data'));
+                    });
+            }
+            }
+        }     
+        else if (req.query.monthly === 'true') {
             if (req.query.words) {
                 sequelize.query('SELECT DATE_FORMAT(time, \'%Y\') AS year, DATE_FORMAT(time, \'%c\') AS month, name, COUNT(*) as count ' +
                                 'FROM Messages as m, People as p, Words as w, MessageWords as mw ' +
