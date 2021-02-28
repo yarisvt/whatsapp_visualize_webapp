9a64f7ef8defdf8889968bce29ce27e350f1b868
Danny van Dortmont <35771251+HighCrit@users.noreply.github.com>
2021-02-28 23:45:39 +0100

    Update database.js

--- api/src/database.js
+++ api/src/database.js
@@ -4,7 +4,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
     host: process.env.DATABASE_HOSTNAME,
     port: process.env.DATABASE_PORT,
-    logging: consola.log,
+    logging: false,
     dialect: 'mariadb'
 });
 
