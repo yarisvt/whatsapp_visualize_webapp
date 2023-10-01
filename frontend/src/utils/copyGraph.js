e4c88e90709516a92fe0d0f51e28fb21d1bec237
yarisvt <yarisvanthiel@gmail.com>
2023-10-01 18:01:26 +0200

    Increase canvas width for copy graph

--- frontend/src/utils/copyGraph.js
+++ frontend/src/utils/copyGraph.js
@@ -3,24 +3,26 @@ import ApexCharts from 'apexcharts';
 const copyGraph = async (chartId, cb) => {
   ApexCharts.exec(chartId, 'dataURI').then(({ imgURI }) => {
     const canvas = document.createElement('canvas');
-    canvas.width = 1100;
+    canvas.width = 1500;
     canvas.height = 400;
 
-    const ctx  = canvas.getContext('2d');
+    const ctx = canvas.getContext('2d');
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     const img = new Image();
     img.src = imgURI;
-    
+
     img.onload = () => {
       ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-      canvas.toBlob(blob => {
-        navigator.clipboard.write([
-        // eslint-disable-next-line no-undef
-          new ClipboardItem({
-            [blob.type]: blob
-          })
-        ]).then(cb)
+      canvas.toBlob((blob) => {
+        navigator.clipboard
+          .write([
+            // eslint-disable-next-line no-undef
+            new ClipboardItem({
+              [blob.type]: blob,
+            }),
+          ])
+          .then(cb)
           .catch(() => {
             alert('Something went wrong while copying the graph');
           });
